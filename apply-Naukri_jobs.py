38a86ebf20694da266b15633a91cad0d869ff7e8
Abyss <198638149+That-Abyss@users.noreply.github.com>
2026-03-30 18:02:51 +0530

    Update apply-Naukri_jobs.py

--- apply-Naukri_jobs.py
+++ apply-Naukri_jobs.py
@@ -74,7 +74,7 @@ def send_telegram_message(message):
             print(f"Telegram Error sending to {chat_id}:", e)
 
 def get_brave_driver():
-    brave_path = r"C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe"  //For Other browser Make changes accordingly
+    brave_path = r"C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe"  #For Other browser Make changes accordingly
     user_data_dir = r"Path for your browser i prefer use Brave"
     options = Options()
     # options.add_argument("--headless=new")  # comment out to see browser
